4c8b98ac1038642cef94c4cbe44d23b3ef32c6a4
Josh Hicks <joshua.brock.hicks@gmail.com>
2024-12-11 10:43:41 -0700

    added middleware

--- main.go
+++ main.go
@@ -7,11 +7,36 @@ import (
 )
 
 func setupRouter() *gin.Engine {
-	r := gin.New()
+	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.GET("/team", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "team.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.GET("/projects", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "team.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.GET("/join", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "join.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.GET("/donate", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "donate.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	r.GET("/contact", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "contact.html", gin.H{
 			"title": "Main website",
 		})
 	})
