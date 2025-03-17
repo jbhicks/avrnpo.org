9e3abd9c586d4ffe9762eaf8d7e3d7b8265d38dd
Joshua Hicks <joshua.brock.hicks@gmail.com>
2025-03-16 22:47:55 -0700

    implemented contact form

--- go.mod
+++ go.mod
@@ -7,10 +7,16 @@ require github.com/gin-gonic/gin v1.10.0
 require github.com/joho/godotenv v1.5.1
 
 require (
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/emersion/go-smtp v0.21.3
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -32,5 +38,6 @@ require (
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/mail.v2 v2.3.1
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,19 +4,157 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/emersion/go-smtp"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+// Add this struct to define the contact form data
+type ContactForm struct {
+	FirstName string `form:"fname"`
+	LastName  string `form:"lname"`
+	Email     string `form:"email"`
+	Message   string `form:"message"`
+}
 
 type HelcimInitializeResponse struct {
 	CheckoutToken string `json:"checkoutToken"`
 	SecretToken   string `json:"secretToken"` // Add SecretToken field
 }
 
+// Define our internal mail message structure
+type Message struct {
+	From    string
+	To      []string
+	Subject string
+	Body    string
+}
+
+// Queue to store messages
+var messageQueue = make(chan Message, 100)
+
+// Simple backend that implements emersion/go-smtp Backend interface
+type Backend struct{}
+
+func (bkd *Backend) NewSession(_ *smtp.Conn) (smtp.Session, error) {
+	return &Session{}, nil
+}
+
+// Session that implements emersion/go-smtp Session interface
+type Session struct {
+	From        string
+	To          []string
+	MessageData []byte // Renamed from Data to MessageData
+}
+
+func (s *Session) AuthPlain(username, password string) error {
+	return nil // No auth needed for internal usage
+}
+
+func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
+	s.From = from
+	return nil
+}
+
+func (s *Session) Rcpt(to string, opts *smtp.RcptOptions) error {
+	s.To = append(s.To, to)
+	return nil
+}
+
+func (s *Session) Data(r io.Reader) error {
+	if b, err := io.ReadAll(r); err != nil {
+		return err
+	} else {
+		s.MessageData = b // Updated to use MessageData instead of Data
+		return nil
+	}
+}
+
+func (s *Session) Reset() {
+	s.From = ""
+	s.To = []string{}
+	s.MessageData = nil // Updated to use MessageData instead of Data
+}
+
+func (s *Session) Logout() error {
+	return nil
+}
+
+// Start our internal SMTP server
+func startSMTPServer() {
+	be := &Backend{}
+
+	s := smtp.NewServer(be)
+	s.Addr = "localhost:1025" // Use a non-standard port
+	s.Domain = "localhost"
+	s.ReadTimeout = 10 * time.Second
+	s.WriteTimeout = 10 * time.Second
+	s.MaxMessageBytes = 1024 * 1024
+	s.MaxRecipients = 50
+	s.AllowInsecureAuth = true
+
+	log.Println("Starting internal SMTP server at", s.Addr)
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Process messages in the queue
+	go processMessageQueue()
+}
+
+// Process messages in the queue
+func processMessageQueue() {
+	for msg := range messageQueue {
+		log.Printf("Sending email to: %v\nSubject: %s\n", msg.To, msg.Subject)
+
+		// Create email message
+		message := fmt.Sprintf("From: %s\r\n"+
+			"To: %s\r\n"+
+			"Subject: %s\r\n"+
+			"\r\n"+
+			"%s\r\n", msg.From, msg.To[0], msg.Subject, msg.Body)
+
+		// Connect to our local SMTP server
+		// No authentication needed for local server
+		err := smtp.SendMail(
+			"localhost:1025",
+			nil, // No auth needed for local server
+			msg.From,
+			msg.To,
+			[]byte(message),
+		)
+
+		if err != nil {
+			log.Printf("Error sending email: %s\n", err)
+		} else {
+			log.Println("Email sent successfully")
+		}
+	}
+}
+
+// Send email function now simply queues the message
+func sendEmail(to []string, subject string, body string, fromEmail string) error {
+	messageQueue <- Message{
+		From:    fromEmail,
+		To:      to,
+		Subject: subject,
+		Body:    body,
+	}
+	return nil
+}
+
 func setupRouter() *gin.Engine {
+	// Start the internal SMTP server
+	startSMTPServer()
+
 	// Set Gin mode based on environment
 	ginMode := os.Getenv("GIN_MODE")
 	if ginMode == "release" {
@@ -70,10 +208,62 @@ func setupRouter() *gin.Engine {
 		})
 	})
 	r.GET("/contact", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "contact.html", gin.H{
-			"title": "Main website",
+		// Check if it's an HTMX request
+		if c.GetHeader("HX-Request") == "true" {
+			// Return just the form
+			c.HTML(http.StatusOK, "contact_form.html", gin.H{
+				"title": "Contact Form",
+			})
+		} else {
+			// Return the full page
+			c.HTML(http.StatusOK, "contact.html", gin.H{
+				"title": "Main website",
+			})
+		}
+	})
+
+	// Add POST handler for contact form
+	r.POST("/contact", func(c *gin.Context) {
+		var form ContactForm
+		if err := c.ShouldBind(&form); err != nil {
+			c.HTML(http.StatusBadRequest, "contact.html", gin.H{
+				"title": "Error",
+				"error": "Invalid form submission. Please try again.",
+			})
+			return
+		}
+
+		// Compose and send email
+		to := []string{"michael@avrnpo.org"}
+		subject := "New Contact Form Submission from AVR Website"
+		body := fmt.Sprintf(
+			"Name: %s %s\nEmail: %s\n\nMessage:\n%s",
+			form.FirstName,
+			form.LastName,
+			form.Email,
+			form.Message,
+		)
+
+		err := sendEmail(to, subject, body, form.Email)
+		if err != nil {
+			fmt.Println("Error sending email:", err)
+		}
+
+		// Return HTML response that will replace the form
+		c.HTML(http.StatusOK, "contact_success.html", gin.H{
+			"firstName": form.FirstName,
 		})
 	})
+
+	// Add a route to view queued messages (admin use only)
+	r.GET("/admin/messages", func(c *gin.Context) {
+		// In a real implementation, you would add authentication here
+		// For now, we just return a placeholder
+		c.JSON(http.StatusOK, gin.H{
+			"message": "This would show queued messages in a real implementation",
+		})
+	})
+
 	r.GET("/footer", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "footer.html", gin.H{
 			"title": "footer",
@@ -146,7 +336,7 @@ func setupRouter() *gin.Engine {
 			return
 		}
 
-		 // Log the checkout token and secret token
+		// Log the checkout token and secret token
 		fmt.Println("Checkout Token:", helcimResponse.CheckoutToken)
 		fmt.Println("Secret Token:", helcimResponse.SecretToken)
 
